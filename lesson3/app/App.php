4adbcc1022ad86cffb5bba0ac0471924b680e7b7
Najmiddin Yodgor <najmiddinyodgor@gmail.com>
2020-11-04 01:53:14 -0500

    Delete port number input feature

--- lesson3/app/App.php
+++ lesson3/app/App.php
@@ -5,21 +5,9 @@ use App\Socket;
 use BracketsChecker\Checker;
 
 class App {
-	public function __construct(string $host, int &$port = null) {
+	public function __construct(string $host, int $port) {
 		$this->host = $host;
-		$this->port = $port ?? $this->getPort();
-	}
-
-	private function getPort() {
-		echo "Enter number of TCP port for listening: ";
-
-		$stream = fopen("php://stdin", 'r');
-		$input = trim(fgets($stream));
-		if(!is_numeric($input)) {
-			$this->getPort();
-		}
-		fclose($stream);
-		return $input;
+		$this->port = $port;
 	}
 
 	public function start() {
